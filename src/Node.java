960cc11764b623563807df846607ccfc5f7520b4
Maya h <7maymay5@gmail.com>
2024-03-14 13:31:57 +0200

    הוספתי את הפעולות שלי לעץ 23 שימימ לב שאת משתמשת בפעולה ISSMALLER על NODEים כי היא חוסכת הרבה (משווה מפתחות)

--- src/Node.java
+++ src/Node.java
@@ -1,18 +1,69 @@
 public class Node<T extends RunnerID> {
     private T key;
     private Runner data;
-    private Node parent;
-    private Node left;
-    private Node middle;
-    private Node right;
-    private int size;
+    private Node<T> parent;
+    private Node<T> left = null;
+    private Node<T> middle = null;
+    private Node<T> right = null;
+    private int size = 0; //check if needs to be 0 or 1
 
+    public Node(){
+        this.key = null;
+        this.data = null;
+        this.parent = null;
+    }
 
-    public Node(T key){
+    public Node(T key, Runner data, Node<T> parent){
         this.key = key;
+        this.data = data;
+        this.parent = parent;
+    }
+
+    public Node<T> getParent() {
+        return parent;
+    }
+
+    public void setParent(Node<T> parent) {
+        this.parent = parent;
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getMiddle() {
+        return middle;
+    }
+
+    public void setMiddle(Node<T> middle) {
+        this.middle = middle;
+    }
+
+    public Node<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
     }
 
     public T getKey() { return this.key; }
+
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 
     public boolean isSmaller(Node<T> other){
         return this.key.isSmaller(other.getKey());
--- src/Tree23.java
+++ src/Tree23.java
@@ -2,52 +2,187 @@ public class Tree23<T extends RunnerID> {
     Node<T> root;
     int size;
 
-    public void init(Tree23<T> Tree){
+    public Tree23(){//m
         //implement from 45
         //O(1)
+        this.root = null;
+        this.size = 0;
     }
 
-    public Node<T> search(Node<T> x, T key){
+    public Node<T> search(Node<T> x, T key){//m
         //implement from 45
         //O(lg(n))
         return null;
     }
 
-    public Node<T> minimum(){
+    public Node<T> minimum(){//m
         //implement from 46
         //O(lg(n))
         return null;
     }
 
-    public void setChildren(Node<T> x, Node<T> l,Node<T> m,Node<T> r){
+    public void updateKey(Node<T> x){//h done
+        Node<T> l = x.getLeft(), m = x.getMiddle(), r = x.getRight();
+        x.setKey(l.getKey());
+        if (m != null){
+            x.setKey(m.getKey());
+        }
+        if (r != null){
+            x.setKey(r.getKey());
+        }
+    }
+
+    public void setChildren(Node<T> x, Node<T> l,Node<T> m,Node<T> r){//h done
         //implement from 47
         //O(lg(n))
-        return;
+        x.setLeft(l);
+        x.setMiddle(m);
+        x.setRight(r);
+        l.setParent(x);
+        if(m != null){
+            m.setParent(x);
+        }
+        if(r != null){
+            r.setParent(x);
+        }
+        updateKey(x);
     }
-    public void insertAndSplit(Node<T> x, Node<T> z){
+    public Node<T> insertAndSplit(Node<T> x, Node<T> z){//h done
         //implement from 48
         //O(lg(n))
-        return;
+        Node<T> l = x.getLeft(), m = x.getMiddle(), r = x.getRight(), y = new Node<T>();
+        if (r == null){
+            if (z.isSmaller(l)) { setChildren(x,z,l,m); }
+            else if (z.isSmaller(m)) { setChildren(x,l,z,m); }
+            else { setChildren(x,l,m,z); }
+            return null;
+        }
+        if (z.isSmaller(l)){
+            setChildren(x,z,l,null);
+            setChildren(y,m,r,null);
+        }
+        else if (z.isSmaller(m)) {
+            setChildren(x,l,z,null);
+            setChildren(y,m,r,null);
+        }
+        else if (z.isSmaller(r)) {
+            setChildren(x,l,m,null);
+            setChildren(y,z,r,null);
+        }
+        else {
+            setChildren(x,l,m,null);
+            setChildren(y,r,z,null);
+        }
+        return y;
     }
 
-    public void insert(Node<T> z){
+    public void insert(Node<T> z){//h done
         //implement from 49
         //O(lg(n))
-        size++;
-        return;
+        Node<T> y = this.root, l = y.getLeft(), m = y.getMiddle(), r = y.getRight(), x;
+        while (!(l == null && m == null && r == null)){
+            if (z.isSmaller(l)) { y = l; }
+            else if (z.isSmaller(m)) { y = m; }
+            else { y = r; }
+            l = y.getLeft(); m = y.getMiddle(); r = y.getRight();
+        }
+        x = y.getParent();
+        z = insertAndSplit(x,z);
+        while (x != this.root) {
+            x = x.getParent();
+            if (z != null) { z = insertAndSplit(x,z); }
+            else { updateKey(x); }
+        }
+        if (z != null) {
+            Node<T> w = new Node<>();
+            setChildren(w,x,z,null);
+            this.root = w;
+        }
+
+        this.size++;
     }
 
-    public void borrowOrMerge(Node<T> y){
+    public Node<T> borrowOrMerge(Node<T> y){//h done
         //implement from 48
         //O(lg(n))
-        return;
+        Node<T> z = y.getParent(), x;
+        if (y == z.getLeft()){
+            x = z.getMiddle();
+            if (x.getRight() != null){
+                setChildren(y, y.getLeft(), x.getLeft(), null);
+                setChildren(x, x.getMiddle(), x.getRight(), null);
+            }
+            else {
+                setChildren(x, y.getLeft(), x.getLeft(), x.getMiddle());
+                delete(y);
+                setChildren(z, x, z.getRight(), null);
+            }
+            return z;
+        }
+        if (y == z.getMiddle()){
+            x = z.getLeft();
+            if (x.getRight() != null){
+                setChildren(y, x.getRight(), y.getLeft(), null);
+                setChildren(x, x.getLeft(), x.getMiddle(), null);
+            }
+            else {
+                setChildren(x, x.getLeft(), x.getMiddle(), y.getLeft());
+                delete(y);
+                setChildren(z, x, z.getRight(), null);
+            }
+            return z;
+        }
+        x = z.getMiddle();
+        if (x.getRight() != null){
+            setChildren(y, x.getRight(), y.getLeft(), null);
+            setChildren(x, x.getLeft(), x.getMiddle(), null);
+        }
+        else{
+            setChildren(x, x.getLeft(), x.getMiddle(), y.getLeft());
+            delete(y);
+            setChildren(z, z.getLeft(), x, null);
+        }
+        return z;
     }
 
-    public void delete(Node<T> x){
+    public void delete(Node<T> x){//h done
         //implement from 51
         //O(lg(n))
+        Node<T> y = x.getParent();
+        if (x == y.getLeft()){
+            setChildren(y, y.getMiddle(), y.getRight(), null);
+        }
+        else if (x == y.getMiddle()) {
+            setChildren(y, y.getLeft(), y.getRight(), null);
+        }
+        else {
+            setChildren(y, y.getLeft(), y.getMiddle(), null);
+        }
+        delete(x);
+        while (y != null){
+            if (y.getMiddle() == null){
+                if (y != this.root){
+                    y = borrowOrMerge(y);
+                }
+                else {
+                    this.root = y.getLeft();
+                    y.getLeft().setParent(null);
+                    delete(y);
+                    return;
+                }
+            }
+            else {
+                updateKey(y);
+                y = y.getParent();
+            }
+        }
+
         size--;
-        return;
+    }
+
+    public int rank(Node<T> x){//m
+        //implement from tutorial 6 , 14
+        return -1;
     }
 
 
